1d04561245dcdd58af36cc3de9e64570caf9821e
nitrofortin <julienstpierrefortin@gmail.com>
2016-03-13 14:37:34 -0400

    applying corrections

--- catsndogs/dataserve.py
+++ catsndogs/dataserve.py
@@ -12,7 +12,7 @@ port = 5555
 
 train = DogsVsCats(('train',), subset=slice(0, 20000))
 
-stream = DataStream.default_stream(
+stream = DataStream(
     train,
     iteration_scheme=ShuffledScheme(train.num_examples, batch_size)
 )
--- catsndogs/model_and_train.py
+++ catsndogs/model_and_train.py
@@ -41,15 +41,15 @@ num_channels = 3
 feature_maps = [20, 50]
 mlp_hiddens = [500]
 conv_step = (1, 1)
-border_mode = 'valid'
+border_mode = 'full'
 
 # Theano variables
 x = tensor.tensor4('image_features')
 y = tensor.lmatrix('targets')
 
 # Conv net
-conv_activation = [Rectifier().apply for _ in feature_maps]
-mlp_activation = [Rectifier().apply for _ in mlp_hiddens] + [Softmax().apply]
+conv_activation = [Rectifier() for _ in feature_maps]
+mlp_activation = [Rectifier() for _ in mlp_hiddens] + [Softmax().apply]
 
 conv_parameters = zip(filter_sizes, feature_maps)
 
@@ -71,109 +71,109 @@ top_mlp_dims = [numpy.prod(conv_sequence.get_dim('output'))] + mlp_hiddens + [ou
 top_mlp = MLP(mlp_activation, mlp_hiddens + [output_size],weights_init=Uniform(0, 0.2),biases_init=Constant(0.))
 top_mlp.initialize()
 
-predict = mlp.apply(out)
+# predict = mlp.apply(out)
 
-cost = CategoricalCrossEntropy().apply(y.flatten(), predict).copy(name='cost')
-error = MisclassificationRate().apply(y.flatten(), predict)
-cg = ComputationGraph([cost, error_rate])
+# cost = CategoricalCrossEntropy().apply(y.flatten(), predict).copy(name='cost')
+# error = MisclassificationRate().apply(y.flatten(), predict)
+# cg = ComputationGraph([cost, error_rate])
 
-image_size = (256,256)
-batch_size = 64
-num_epochs = 60
-save_to = "CatsVsDogs.pkl"
+# image_size = (256,256)
+# batch_size = 64
+# num_epochs = 60
+# save_to = "CatsVsDogs.pkl"
 
-train = DogsVsCats(('train',), subset=slice(0, 20000))
-valid = DogsVsCats(('train',), subset=slice(20000,25000))
+# train = DogsVsCats(('train',), subset=slice(0, 20000))
+# valid = DogsVsCats(('train',), subset=slice(20000,25000))
 
-train_stream = DataStream.default_stream(
-    train,
-    iteration_scheme=ShuffledScheme(train.num_examples, batch_size)
-)
+# train_stream = DataStream.default_stream(
+#     train,
+#     iteration_scheme=ShuffledScheme(train.num_examples, batch_size)
+# )
 
-downscale_train_stream = MinimumImageDimensions(
-	data_stream = train_stream,
-	minimum_shape = image_size, 
-	which_sources=('image_features',)
-)
+# downscale_train_stream = MinimumImageDimensions(
+# 	data_stream = train_stream,
+# 	minimum_shape = image_size, 
+# 	which_sources=('image_features',)
+# )
 
-upscale_train_stream = MaximumImageDimensions(
-	data_stream = downscale_train_stream, 
-	maximum_shape = image_size, 
-	which_sources=('image_features',)
-)
+# upscale_train_stream = MaximumImageDimensions(
+# 	data_stream = downscale_train_stream, 
+# 	maximum_shape = image_size, 
+# 	which_sources=('image_features',)
+# )
 
-rotated_train_stream = Random2DRotation(
-	data_stream = upscale_train_stream, 
-	which_sources=('image_features',)
-)
+# rotated_train_stream = Random2DRotation(
+# 	data_stream = upscale_train_stream, 
+# 	which_sources=('image_features',)
+# )
 
-scaled_train_stream = ScaleAndShift(
-	data_stream = rotated_train_stream, 
-	scale = 1./255, 
-	shift = 0, 
-	which_sources = ('image_features',)
-)
+# scaled_train_stream = ScaleAndShift(
+# 	data_stream = rotated_train_stream, 
+# 	scale = 1./255, 
+# 	shift = 0, 
+# 	which_sources = ('image_features',)
+# )
 
-data_train_stream = Cast(
-	data_stream = scaled_train_stream, 
-	dtype = 'float32', 
-	which_sources = ('image_features',)
-)
+# data_train_stream = Cast(
+# 	data_stream = scaled_train_stream, 
+# 	dtype = 'float32', 
+# 	which_sources = ('image_features',)
+# )
 
-valid_stream = DataStream.default_stream(
-    valid,
-    iteration_scheme=ShuffledScheme(valid.num_examples, batch_size)
-)
+# valid_stream = DataStream.default_stream(
+#     valid,
+#     iteration_scheme=ShuffledScheme(valid.num_examples, batch_size)
+# )
 
-downscale_valid_stream = MinimumImageDimensions(
-	data_stream = valid_stream,
-	minimum_shape = image_size, 
-	which_sources=('image_features',)
-)
+# downscale_valid_stream = MinimumImageDimensions(
+# 	data_stream = valid_stream,
+# 	minimum_shape = image_size, 
+# 	which_sources=('image_features',)
+# )
 
-upscale_valid_stream = MaximumImageDimensions(
-	data_stream = downscale_valid_stream, 
-	maximum_shape = image_size, 
-	which_sources=('image_features',)
-)
+# upscale_valid_stream = MaximumImageDimensions(
+# 	data_stream = downscale_valid_stream, 
+# 	maximum_shape = image_size, 
+# 	which_sources=('image_features',)
+# )
 
-rotated_valid_stream = Random2DRotation(
-	data_stream = upscale_valid_stream, 
-	which_sources=('image_features',)
-)
+# rotated_valid_stream = Random2DRotation(
+# 	data_stream = upscale_valid_stream, 
+# 	which_sources=('image_features',)
+# )
 
-scaled_valid_stream = ScaleAndShift(
-	data_stream = rotated_valid_stream, 
-	scale = 1./255, 
-	shift = 0, 
-	which_sources = ('image_features',)
-)
+# scaled_valid_stream = ScaleAndShift(
+# 	data_stream = rotated_valid_stream, 
+# 	scale = 1./255, 
+# 	shift = 0, 
+# 	which_sources = ('image_features',)
+# )
 
-data_valid_stream = Cast(
-	data_stream = scaled_valid_stream, 
-	dtype = 'float32', 
-	which_sources = ('image_features',)
-)
-
-
-algorithm = GradientDescent(cost=cost, parameters=cg.parameters, step_rule=Scale(learning_rate=0.1))
+# data_valid_stream = Cast(
+# 	data_stream = scaled_valid_stream, 
+# 	dtype = 'float32', 
+# 	which_sources = ('image_features',)
+# )
 
 
-extensions = [Timing(),
-              FinishAfter(after_n_epochs=num_epochs),
-              DataStreamMonitoring(
-                  [cost, error_rate],
-                  data_valid_stream,
-                  prefix="valid"),
-              TrainingDataMonitoring(
-                  [cost, error_rate,
-                   aggregation.mean(algorithm.total_gradient_norm)],
-                  prefix="train",
-                  after_epoch=True),
-              Checkpoint(save_to),
-              ProgressBar(),
-              Printing()]
+# algorithm = GradientDescent(cost=cost, parameters=cg.parameters, step_rule=Scale(learning_rate=0.1))
 
-model = Model(cost)
-main_loop = MainLoop(algorithm,data_stream=data_train_stream,model=model,extensions=extensions)
-main_loop.run()
+
+# extensions = [Timing(),
+#               FinishAfter(after_n_epochs=num_epochs),
+#               DataStreamMonitoring(
+#                   [cost, error_rate],
+#                   data_valid_stream,
+#                   prefix="valid"),
+#               TrainingDataMonitoring(
+#                   [cost, error_rate,
+#                    aggregation.mean(algorithm.total_gradient_norm)],
+#                   prefix="train",
+#                   after_epoch=True),
+#               Checkpoint(save_to),
+#               ProgressBar(),
+#               Printing()]
+
+# model = Model(cost)
+# main_loop = MainLoop(algorithm,data_stream=data_train_stream,model=model,extensions=extensions)
+# main_loop.run()

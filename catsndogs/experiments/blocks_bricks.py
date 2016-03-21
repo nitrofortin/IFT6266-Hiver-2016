c57b4fe8e7b6ef005ef3de76dc917bf0288c205c
nitrofortin <julienstpierrefortin@gmail.com>
2016-03-21 13:35:20 -0400

    modify ELU

--- catsndogs/experiments/blocks_bricks.py
+++ catsndogs/experiments/blocks_bricks.py
@@ -1,4 +1,5 @@
 """Some of the simplest individual bricks."""
+# Must be enhance to allow setting custom alpha parameter at initialization.
 import logging
 
 from theano import tensor
@@ -13,7 +14,8 @@ from blocks.utils import shared_floatx_nans
 
 logger = logging.getLogger(__name__)
 
-class ELU(Activation):
+class ELU(Activation):	
     @application(inputs=['input_'], outputs=['output'])
     def apply(self, input_):
-        return tensor.switch(input_ >= 0, input_, tensor.exp(input_) - 1)
+    	self.alpha = 1.0
+        return tensor.switch(input_ >= 0, input_, self.alpha*(tensor.exp(input_) - 1))
